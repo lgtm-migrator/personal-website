4f625968495c0d8091b0c58e025e83aa2e3a7d1b
Lukas <lukas.stauersbol@gmail.com>
2021-11-04 10:14:03 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
         <h1>Welcome</h1>
-        <p>testtt3s</p>
+        <p>testtt3ss</p>
       </v-card>
     </v-col>
   </v-row>
