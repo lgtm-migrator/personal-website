d6f8c8255034b20e839e8fc56abdce84c9eafaab
Lukas <lukas.stauersbol@gmail.com>
2021-11-04 09:41:13 +0100

    Maybe fixed CI problems

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
+        <h1>Welcome</h1>
       </v-card>
     </v-col>
   </v-row>
