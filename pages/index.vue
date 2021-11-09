5ab44ab4a947622ac408e3c4845a1487b83bcc31
Lukas <lukas.stauersbol@gmail.com>
2021-11-09 08:01:44 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     </v-col>
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ut facere tenetur dolor debitis, sint nihil odit possimus quam sequi velit neque! Inventore esse dignissimos perferendis, vero rerum ipsam odit.</p>
       </v-card>
     </v-col>
   </v-row>
