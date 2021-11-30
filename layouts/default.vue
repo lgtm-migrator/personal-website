08a24f4ddb2c49cee3ee8b639d46a63e31be4984
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-11-30 10:52:49 +0100

    Fixed stupid bugg

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,7 +28,7 @@
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
