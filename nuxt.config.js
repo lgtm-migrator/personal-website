8f0e9ef0bb9bac3e8648eaf2bc74aaa699522a3f
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-12-06 07:56:56 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/particle.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
