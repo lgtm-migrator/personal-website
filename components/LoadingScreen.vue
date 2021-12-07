9fb61a2f4c3641f7d2044d13da6f5f6de64d2159
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-12-07 10:27:43 +0100

    feat: Adding loading screen
    Removed unused components and added more functionality to loading screen

--- components/LoadingScreen.vue
+++ components/LoadingScreen.vue
@@ -1,24 +1,34 @@
 <template>
-  <div>
-
+  <div v-if="loading" class="loading-page">
+    <div class="loading"></div>
   </div>
 </template>
 
 <script>
+import Lottie from 'lottie-web/build/player/lottie'
 export default {
-
   data () {
     return {
-
+      loading: false
     }
   },
 
   mounted () {
-
+    Lottie.loadAnimation({
+      container: this.$refs.loading,
+      renderer: 'svg',
+      loop: true,
+      path: ''
+    })
   },
 
   methods: {
-
+    start () {
+      this.loading = true
+    },
+    finish () {
+      this.loading = false
+    }
   }
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -99,5 +99,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  loading: '~/components/LoadingScreen.vue',
 }
